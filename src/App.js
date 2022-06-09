45d15effe06d8ad54fd12a95d99ee86664369fa0
Oleksandr Kobzar <kobzaravi@gmail.com>
2022-06-09 14:02:47 +0300

    created component converter

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from "react";
+import Conwerter from "./components/converter";
 
 function App() {
   return (
-    <div />
+    <div className="wrapper">
+        <h1>Carrency-Conwerter</h1>
+        <Conwerter />
+    </div>
   );
 }
 
---
+++ src/components/converter.js
@@ -0,0 +1,28 @@
+import React from "react";
+import bootstrap from "bootstrap";
+
+export default function Conwerter () {
+    return (
+        <table>
+           
+<table class="table-primary">...</table>
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Amount</th>
+      <th scope="col">From</th>
+      <th scope="col">To</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>"____"</td>
+      <td>USD</td>
+      <td>UAH</td>
+    </tr>
+  </tbody>
+  <button type="submit">To count</button>
+</table>       
+    )
+}
